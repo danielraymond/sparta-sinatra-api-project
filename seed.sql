2e6a7ab17a5c87596c16c28125992c6946df15ed
Tech-A08 <tech-a08@Tech-A08s-MacBook-Air.local>
2018-01-05 22:32:12 +0000

    finished tests for resource controller

--- seed.sql
+++ seed.sql
@@ -15,3 +15,4 @@ CREATE TABLE object (
 INSERT INTO object (title , object_type , radius , mass , gravity , planet) VALUES ('Earth' , 'Planet' , 1.0 , 1.0 , 9.81 , null);
 INSERT INTO object (title , object_type , radius , mass , gravity , planet) VALUES ('The Moon' , 'Moon' , 0.2727 , 0.0123 , 1.625 , 'Earth');
 INSERT INTO object (title , object_type , radius , mass , gravity , planet) VALUES ('Jupiter' , 'Planet' , 10.97 , 317.83 , 24.79 , null);
+INSERT INTO object (title , object_type , radius , mass , gravity , planet) VALUES ('To be deleted' , 'Planet' , 2.0 , 2.0 , 2.0 , null);
--- spec/solar_objects_controller_spec.rb
+++ spec/solar_objects_controller_spec.rb
@@ -27,24 +27,42 @@ describe 'Solar Object Controller' do
   end
 
   it "displays individual solar objects page" do
-    get '/index/1'
+    get '/index/2'
     expect(last_response).to be_ok
     expect(last_response.body).to include('show-page')
   end
 
   it "displays the edit solar object page" do
-    get '/index/1/edit'
+    get '/index/2/edit'
     expect(last_response).to be_ok
     expect(last_response.body).to include('editPlanet')
   end
 
   it "post request to create a new solar object should work" do
-    get '/index/new'
-    post '/index', :object => {:title => 'Test', :object_type => 'Planet', :radius => 1.23, :mass => 4.56, :gravity => 7.89, :planet => ''}
+    params = {:title => 'Test for post', :object_type => 'Planet', :radius => 1.0, :mass => 1.0, :gravity => 9.81, :planet => ''}
+    post '/index', params
     expect(last_response).to be_redirect
     follow_redirect!
-    # expect(last_response.body).to include('Solar Objects')
+    expect(last_response.body).to include('Test for post')
+  end
 
+  it "put request should update an existing solar object" do
+    params = params = {:title => 'Earth-update', :object_type => 'Planet', :radius => 1.23, :mass => 4.56, :gravity => 7.89, :planet => ''}
+    put '/index/4', params
+    expect(last_response).to be_redirect
+    follow_redirect!
+    expect(last_response.body).to include('Earth-update')
+  end
+
+  it "delete request should delete a solar object" do
+    delete '/index/4'
+    expect(last_response).to be_redirect
+    follow_redirect!
+    expect(last_response.body).not_to include('Earth-update')
+    delete '/index/5'
+    expect(last_response).to be_redirect
+    follow_redirect!
+    expect(last_response.body).not_to include('Test for post')
   end
 
 end
